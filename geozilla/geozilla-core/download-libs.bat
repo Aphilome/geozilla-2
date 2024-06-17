83ea82469a8d43a8cc0a13716f89a43a23d5ee30
igor.fedorov <igor.fedorov@my.games>
2024-06-18 00:18:45 +0300

    set vcpkg triplet

--- geozilla/geozilla-core/download-libs.bat
+++ geozilla/geozilla-core/download-libs.bat
@@ -7,4 +7,4 @@ echo Updating vcpkg
 call .\external\vcpkg\bootstrap-vcpkg.bat
 
 echo Installing vcpkg libs
-call .\external\vcpkg\vcpkg install pcl
+call .\external\vcpkg\vcpkg install pcl --triplet=x64-windows
