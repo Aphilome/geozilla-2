a244b825eafe94dfb5b86997b7f1d4dda4a51777
igor.fedorov <igor.fedorov@astrum.team>
2024-06-19 14:00:14 +0300

    add pcl visualization

--- geozilla/geozilla-core/source/core/GeozillaCore.cpp
+++ geozilla/geozilla-core/source/core/GeozillaCore.cpp
@@ -7,6 +7,15 @@
 
 #include <CesiumGltf/Model.h>
 
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/filter_indices.h> // for pcl::removeNaNFromPointCloud
+#include <pcl/segmentation/region_growing.h>
+
 #include <algorithm>
 
 namespace
@@ -52,10 +61,65 @@ const char* ConvertToRawMemory(const std::string& data)
 const char* GenerateGeoJson(const char* path, float latitude, float longitude)
 {
     auto models = LoadGeoModels(path);
-    auto pointCloud = ConvertToPointCloud(models);
+    auto cloud = ConvertToPointCloud(models);
+
+    {
+        pcl::search::Search<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+        pcl::PointCloud <pcl::Normal>::Ptr normals(new pcl::PointCloud <pcl::Normal>);
+        pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
+        normal_estimator.setSearchMethod(tree);
+        normal_estimator.setInputCloud(cloud);
+        normal_estimator.setKSearch(50);
+        normal_estimator.compute(*normals);
+
+        pcl::IndicesPtr indices(new std::vector <int>);
+        pcl::removeNaNFromPointCloud(*cloud, *indices);
+
+        pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
+        reg.setMinClusterSize(50);
+        reg.setMaxClusterSize(1000000);
+        reg.setSearchMethod(tree);
+        reg.setNumberOfNeighbours(30);
+        reg.setInputCloud(cloud);
+        reg.setIndices(indices);
+        reg.setInputNormals(normals);
+        reg.setSmoothnessThreshold(3.0 / 180.0 * M_PI);
+        reg.setCurvatureThreshold(1.0);
+
+        std::vector <pcl::PointIndices> clusters;
+        reg.extract(clusters);
+
+        //pcl::io::savePCDFileASCII("cloud.pcd", *cloud);
+
+        for (size_t i = 0; i < clusters.size(); ++i)
+        {
+            auto c = std::make_shared<gz::core::GltfToPointCloudConverter::Points>();
+            for (auto j : clusters[i].indices)
+            {
+                c->push_back(cloud->points[j]);
+            }
+
+            pcl::visualization::CloudViewer viewer1("Cluster viewer");
+            viewer1.showCloud(c);
+            while (!viewer1.wasStopped())
+            {
+            }
+
+            //pcl::io::savePCDFileASCII("cluster" + std::to_string(i) + ".pcd", *c);
+        }
+
+        pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud();
+        pcl::visualization::CloudViewer viewer("Cluster viewer");
+        viewer.showCloud(colored_cloud);
+        while (!viewer.wasStopped())
+        {
+        }
+
+        std::cout << clusters.size() << std::endl;
+    }
 
     auto splitter = ZoneSplitter();
-    std::string result = splitter.SplitToZones(pointCloud);
+    std::string result = "{}";//splitter.SplitToZones(pointCloud);
     return ConvertToRawMemory(result);
 }
 
