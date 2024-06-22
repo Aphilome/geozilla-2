a270bf4f6b4785b13edaeac8149e69110b0de5e5
igor.fedorov <igor.fedorov@astrum.team>
2024-06-22 20:01:52 +0300

    fix tex coord extraction

--- geozilla/geozilla-core/source/core/Algorithm/GltfToPointCloudConverter.h
+++ geozilla/geozilla-core/source/core/Algorithm/GltfToPointCloudConverter.h
@@ -2,6 +2,8 @@
 
 #include "GeoTypes.h"
 
+#include <array>
+
 namespace gz::core
 {
 
@@ -14,6 +16,11 @@ private:
     static PointCloud::Ptr ExtractPoints(const GeoModel& model);
     static glm::dvec3 ExtractCenter(const GeoModel& model);
     static PointCloud::Ptr Normalize(const PointCloud::Ptr& points, const glm::dvec3& center);
+    static const CesiumGltf::Accessor* GetAccessor(const GeoModel& model, const std::unordered_map<std::string, int32_t>& attributes, const std::string& name);
+    static const std::byte* GetAttributeBuffer(const GeoModel& model, const CesiumGltf::Accessor* accessor);
+    static const CesiumGltf::TextureInfo* GetTextureInfo(const GeoModel& model, int32_t materialIndex);
+    static const CesiumGltf::Image* GetImage(const GeoModel& model, const CesiumGltf::TextureInfo* textureInfo);
+    static std::array<std::uint8_t, 3> GetColor(const CesiumGltf::Image* image, float u, float v);
 };
 
 } // namespace gz::core
